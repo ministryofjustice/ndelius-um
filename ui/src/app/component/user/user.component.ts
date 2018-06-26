d065f1ef375df600e1e4e5ad98fbdf9ce2e3a02f
Marcus.Aspin <Marcus.Aspin@bconline.co.uk>
2018-06-26 12:18:56 +0100

    Added dummy user lookup to view user screen

--- ui/src/app/component/user/user.component.ts
+++ ui/src/app/component/user/user.component.ts
@@ -1,10 +1,21 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
 import {User} from "../../model/User";
+import {ActivatedRoute} from "@angular/router";
+import {flatMap} from "rxjs/operators";
+import {UserService} from "../../service/user.service";
 
 @Component({
   selector: 'user',
   templateUrl: './user.component.html'
 })
-export class UserComponent {
-  user: User = new User();
+export class UserComponent implements OnInit {
+  user: User;
+
+  constructor(private route: ActivatedRoute, private service: UserService) {}
+
+  ngOnInit(): void {
+    this.route.params
+      .pipe(flatMap(params => this.service.user(params.id)))
+      .subscribe((user: User) => this.user = user);
+  }
 }
--- ui/src/app/service/user.service.ts
+++ ui/src/app/service/user.service.ts
@@ -4,4 +4,5 @@ import {Observable} from "rxjs/Observable";
 export abstract class UserService {
   abstract whoami(): Observable<User>;
   abstract users(query: string, page: number): Observable<User[]>;
+  abstract user(username: string): Observable<User>;
 }
