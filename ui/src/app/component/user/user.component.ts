557586def71ba5795b1cb946c3c7c0fb38387ea4
Marcus.Aspin <Marcus.Aspin@bconline.co.uk>
2018-07-13 10:03:16 +0100

    Initial view user screen

--- ui/src/app/component/user/user.component.ts
+++ ui/src/app/component/user/user.component.ts
@@ -3,20 +3,36 @@ import {User} from "../../model/User";
 import {ActivatedRoute} from "@angular/router";
 import {flatMap} from "rxjs/operators";
 import {UserService} from "../../service/user.service";
+import {Observable} from "rxjs/Observable";
+import {AuthorisationService} from "../../service/impl/authorisation.service";
 
 @Component({
   selector: 'user',
   templateUrl: './user.component.html'
 })
 export class UserComponent implements OnInit {
+  loaded: boolean;
+
+  mode: string;
   user: User;
 
-  constructor(private route: ActivatedRoute, private service: UserService) {}
+  constructor(private route: ActivatedRoute, private service: UserService, public auth: AuthorisationService) {}
 
   ngOnInit(): void {
     this.route.params
-      .pipe(flatMap(params => this.service.user(params.id)))
-      .subscribe((user: User) => this.user = user);
+      .pipe(flatMap(params => {
+        if (params.id != null) {
+          this.mode = this.auth.canUpdateUser()? 'Update': 'View';
+          return this.service.user(params.id)
+        } else {
+          this.mode = 'Add';
+          return Observable.create(new User());
+        }
+      }))
+      .subscribe((user: User) => {
+        this.user = user;
+        this.loaded = true;
+      });
   }
 
   get json() {
