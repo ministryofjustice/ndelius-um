f8bb05aade515a419605d9a087d29ef6aafefdd3
Peter Wilson <peter.wilson@bconline.co.uk>
2018-07-20 14:56:10 +0100

    get role-groups api

--- src/main/java/uk/co/bconline/ndelius/service/RoleGroupService.java
+++ src/main/java/uk/co/bconline/ndelius/service/RoleGroupService.java
@@ -2,7 +2,10 @@ package uk.co.bconline.ndelius.service;
 
 import uk.co.bconline.ndelius.model.TransactionGroup;
 
+import java.util.Optional;
+
 public interface RoleGroupService
 {
     Iterable<TransactionGroup> getTransactionGroups();
+    Optional<TransactionGroup> getTransactionGroup(String transactionGroupName);
 }
