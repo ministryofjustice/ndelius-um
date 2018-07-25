d3049578f334a9ffd3a0e739caa6155349c685da
Marcus.Aspin <Marcus.Aspin@bconline.co.uk>
2018-07-25 16:11:48 +0100

    Added definition for error response JSON

--- src/test/java/uk/co/bconline/ndelius/advice/ControllerExceptionHandlerTest.java
+++ src/test/java/uk/co/bconline/ndelius/advice/ControllerExceptionHandlerTest.java
@@ -3,35 +3,34 @@ package uk.co.bconline.ndelius.advice;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
-import java.util.Map;
-
 import org.junit.Test;
 
 import uk.co.bconline.ndelius.exception.AppException;
+import uk.co.bconline.ndelius.model.ErrorResponse;
 
 public class ControllerExceptionHandlerTest
 {
 	@Test
 	public void appExceptionReturns500ErrorWithMessage()
 	{
-		Map response = new ControllerExceptionHandler().handle(new AppException("test message"));
-		assertEquals("test message", response.get("error"));
+		ErrorResponse response = new ControllerExceptionHandler().handle(new AppException("test message"));
+		assertEquals("test message", response.getError().get(0));
 	}
 
 	@Test
 	public void appExceptionDoesntIncludeStackTraceIfMessageProvided()
 	{
-		Map response = new ControllerExceptionHandler().handle(new AppException("test message", new RuntimeException()));
-		assertEquals("test message", response.get("error"));
+		ErrorResponse response = new ControllerExceptionHandler().handle(new AppException("test message", new RuntimeException()));
+		assertEquals("test message", response.getError().get(0));
 
 		response = new ControllerExceptionHandler().handle(new AppException(new RuntimeException()));
-		assertEquals("java.lang.RuntimeException", response.get("error"));
+		assertEquals("java.lang.RuntimeException", response.getError().get(0));
 	}
 
 	@Test
 	public void appExceptionEmptyMessageReturnsEmptyBody()
 	{
-		Map response = new ControllerExceptionHandler().handle(new AppException());
+		ErrorResponse response = new ControllerExceptionHandler().handle(new AppException());
 		assertNull(response);
 	}
 }
