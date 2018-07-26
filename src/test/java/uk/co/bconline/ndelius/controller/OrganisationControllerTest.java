3d7ab590355c861d95a5df383b8054b24a75881f
Joseph.Dundon <joseph.dundon@bconline.co.uk>
2018-07-26 11:16:14 +0100

    Added organisations API

---
+++ src/test/java/uk/co/bconline/ndelius/controller/OrganisationControllerTest.java
@@ -0,0 +1,61 @@
+package uk.co.bconline.ndelius.controller;
+
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static uk.co.bconline.ndelius.test.util.AuthUtils.token;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@RunWith(SpringRunner.class)
+
+public class OrganisationControllerTest
+{
+	@Autowired
+	private WebApplicationContext context;
+
+	@Autowired
+	private BasicAuthenticationFilter basicAuthenticationFilter;
+
+	@Autowired
+	private OncePerRequestFilter jwtAuthenticationFilter;
+
+	private MockMvc mvc;
+
+	@Before
+	public void setup()
+	{
+		mvc = MockMvcBuilders
+				.webAppContextSetup(context)
+				.addFilter(jwtAuthenticationFilter)
+				.addFilter(basicAuthenticationFilter)
+				.alwaysDo(print())
+				.build();
+	}
+
+	@Test
+	public void allOrganisationsAreReturned() throws Exception
+	{
+		mvc.perform(get("/api/organisations")
+				.header("Authorization", "Bearer " + token(mvc)))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$", hasSize(3)))
+				.andExpect(jsonPath("$[*].code", hasItems("NPS","PO1","PO2")));
+	}
+}
