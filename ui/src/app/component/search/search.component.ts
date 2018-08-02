82f4fc8f439ad51f948a74b23c04c5aacb4e5db4
Marcus.Aspin <Marcus.Aspin@bconline.co.uk>
2018-08-02 16:03:09 +0100

    Added search on type, loading indicator + no results message

--- ui/src/app/component/search/search.component.ts
+++ ui/src/app/component/search/search.component.ts
@@ -1,35 +1,49 @@
-import {Component, OnInit} from "@angular/core";
+import {AfterViewInit, Component, OnInit, ViewChild} from "@angular/core";
 import {User} from "../../model/user";
 import {ActivatedRoute, Router} from "@angular/router";
-import {filter, flatMap} from "rxjs/operators";
+import {debounceTime, distinctUntilChanged, filter, flatMap, tap} from "rxjs/operators";
 import {UserService} from "../../service/user.service";
 import {AuthorisationService} from "../../service/impl/authorisation.service";
+import {NgModel} from "@angular/forms";
 
 @Component({
   selector: 'search',
   providers: [AuthorisationService],
   templateUrl: './search.component.html'
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent implements OnInit, AfterViewInit {
+  @ViewChild('queryInput') queryInput: NgModel;
   query: string = "";
   page: number;
+
   users: User[] = [];
-  hasMoreResults: boolean = false;
+  hasMoreResults: boolean;
+  searching: boolean;
+  noResults: boolean;
 
   constructor(private route: ActivatedRoute, private router: Router, private service: UserService, public auth: AuthorisationService) {
   }
 
   ngOnInit(): void {
     this.route.queryParams.pipe(
-      filter(params => params.q != null),
+      filter(params => params.q != null && params.q !== ""),
+      tap(() => this.searching = true),
       flatMap(params => this.service.search(
         this.query = params.q,
         this.page = +params.page || 1
       ))
     ).subscribe(users => {
-      this.hasMoreResults = users.length >= 10;
       this.users = users;
+      this.hasMoreResults = users.length >= 10;
+      this.noResults = users.length === 0;
+      this.searching = false;
     });
+  }
+
+  ngAfterViewInit(): void {
+    this.queryInput.valueChanges
+      .pipe(debounceTime(500), distinctUntilChanged())
+      .subscribe(() => this.search());
   }
 
   search() {
