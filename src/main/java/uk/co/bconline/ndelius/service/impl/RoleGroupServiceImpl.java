c992ead035a8bdbf025cf99bc1e4beeb5bd747a6
Marcus.Aspin <Marcus.Aspin@bconline.co.uk>
2018-08-03 10:03:08 +0100

    Filter role groups to only include roles based on access to sector, adminlevel, level 1/2/3.

--- src/main/java/uk/co/bconline/ndelius/service/impl/RoleGroupServiceImpl.java
+++ src/main/java/uk/co/bconline/ndelius/service/impl/RoleGroupServiceImpl.java
@@ -1,6 +1,7 @@
 package uk.co.bconline.ndelius.service.impl;
 
 import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toSet;
 import static java.util.stream.StreamSupport.stream;
 import static org.springframework.ldap.query.LdapQueryBuilder.query;
 import static org.springframework.ldap.query.SearchScope.ONELEVEL;
@@ -12,6 +13,7 @@ import org.springframework.ldap.odm.annotations.Entry;
 import org.springframework.stereotype.Service;
 
 import lombok.val;
+import uk.co.bconline.ndelius.model.Role;
 import uk.co.bconline.ndelius.model.RoleGroup;
 import uk.co.bconline.ndelius.model.ldap.OIDRoleGroup;
 import uk.co.bconline.ndelius.repository.oid.OIDRoleGroupRepository;
@@ -20,7 +22,8 @@ import uk.co.bconline.ndelius.service.RoleService;
 import uk.co.bconline.ndelius.transformer.RoleGroupTransformer;
 
 @Service
-public class RoleGroupServiceImpl implements RoleGroupService {
+public class RoleGroupServiceImpl implements RoleGroupService
+{
     private final OIDRoleGroupRepository oidRoleGroupRepository;
     private final RoleGroupTransformer roleGroupTransformer;
     private final RoleService roleService;
@@ -34,21 +37,33 @@ public class RoleGroupServiceImpl implements RoleGroupService {
 
     public Iterable<RoleGroup> getRoleGroups()
     {
+		val rolesICanAssign = stream(roleService.getRoles().spliterator(), false).map(Role::getName).collect(toSet());
         return stream(oidRoleGroupRepository
                 .findAll(query()
                         .searchScope(ONELEVEL)
                         .base(OIDRoleGroup.class.getAnnotation(Entry.class).base())
                         .where("objectclass").like("*")).spliterator(), false)
+				.peek(g -> g.setRoles(roleService
+						.getRolesByParent(g.getName(), OIDRoleGroup.class).stream()
+						.filter(role -> rolesICanAssign.contains(role.getName()))
+						.collect(toList())))
+				.filter(g -> !g.getRoles().isEmpty())
                 .map(roleGroupTransformer::map)
                 .collect(toList());
     }
 
     @Override
-    public Optional<RoleGroup> getRoleGroup(String name) {
-        val group = oidRoleGroupRepository.findByName(name);
-        return group.map(g -> {
-            g.setRoles(roleService.getRolesByParent(name, OIDRoleGroup.class));
-            return g;
-        }).map(roleGroupTransformer::map);
+    public Optional<RoleGroup> getRoleGroup(String name)
+	{
+		val rolesICanAssign = stream(roleService.getRoles().spliterator(), false).map(Role::getName).collect(toSet());
+        return oidRoleGroupRepository.findByName(name)
+				.map(g -> {
+					g.setRoles(roleService
+							.getRolesByParent(name, OIDRoleGroup.class).stream()
+							.filter(role -> rolesICanAssign.contains(role.getName()))
+							.collect(toList()));
+					return g;
+				})
+				.map(roleGroupTransformer::map);
     }
 }
