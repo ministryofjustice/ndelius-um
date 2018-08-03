456b877a3aeee0b1f4c2ecc79ba3deb40e23d996
Joseph.Dundon <joseph.dundon@bconline.co.uk>
2018-08-03 13:56:22 +0100

    Added some formatting to the api errors on the front end.

--- ui/src/app/interceptor/error.interceptor.ts
+++ ui/src/app/interceptor/error.interceptor.ts
@@ -16,7 +16,15 @@ export class ErrorInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(req).pipe(tap(()=>{},(res: HttpResponseBase) => {
       if (res instanceof HttpErrorResponse) {
-        AppComponent.globalMessage = res.message;
+        let error: string = JSON.stringify(res.error);
+        if (res.error.error instanceof Array) {
+          error = res.error.error.join(", ");
+        }
+        if(res.error.requiredRoles instanceof Array){
+          error = " Access denied. Roles required: " + res.error.requiredRoles.join(", ")
+        }
+
+        AppComponent.globalMessage = "Error " + res.status + " | " + error;
         AppComponent.globalMessageSeverity = "danger";
       }
     }));
