3d4780336e6e04990e91b6d0bbb10e9b429d3854
Marcus.Aspin <Marcus.Aspin@bconline.co.uk>
2018-08-10 14:53:20 +0100

    Fix for 500 error when organisation is not populated in homeArea

--- src/test/java/uk/co/bconline/ndelius/controller/UserControllerTest.java
+++ src/test/java/uk/co/bconline/ndelius/controller/UserControllerTest.java
@@ -375,4 +375,32 @@ public class UserControllerTest
 				.andExpect(jsonPath("$.roles", hasSize(1)))
 				.andExpect(jsonPath("$.roles[0].name", is("UMBT002")));
 	}
+
+
+	@Test
+	public void organisationIsDerivedFromHomeArea() throws Exception
+	{
+		String token = token(mvc);
+		mvc.perform(post("/api/user")
+				.header("Authorization", "Bearer " + token)
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(new ObjectMapper().findAndRegisterModules().writeValueAsString(User.builder()
+						.username("test.user5")
+						.forenames("Test")
+						.surname("User5")
+						.privateSector(false)
+						.startDate(LocalDate.of(2000, 1, 1))
+						.datasets(asList(
+								Dataset.builder().code("N01").build(),
+								Dataset.builder().code("N02").build()))
+						.homeArea(Dataset.builder().code("N01").build())
+						.build())))
+				.andExpect(status().isCreated())
+				.andExpect(redirectedUrl("/user/test.user5"));
+
+		mvc.perform(get("/api/user/test.user5")
+				.header("Authorization", "Bearer " + token))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.homeArea.organisation.code", is("NPS")));
+	}
 }
