231db025839f694a6183db53dbdd22b7b256ac89
Marcus.Aspin <Marcus.Aspin@bconline.co.uk>
2018-08-14 12:26:22 +0100

    Fix failing test due to change in JWT secret management

--- src/test/java/uk/co/bconline/ndelius/util/JwtHelperTest.java
+++ src/test/java/uk/co/bconline/ndelius/util/JwtHelperTest.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -24,6 +25,9 @@ public class JwtHelperTest
 {
 	@Autowired
 	private JwtHelper jwtHelper;
+
+	@Value("${jwt.secret}")
+	private String secret;
 
 	@Test
 	public void tokenCanBeRetrievedFromHeader()
@@ -61,7 +65,7 @@ public class JwtHelperTest
 	{
 		return Jwts.builder()
 				.setSubject(sub)
-				.signWith(SignatureAlgorithm.HS512, "secret")
+				.signWith(SignatureAlgorithm.HS512, secret)
 				.compact();
 	}
 }
