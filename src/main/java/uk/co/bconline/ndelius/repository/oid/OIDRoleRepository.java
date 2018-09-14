24ef82ebac9a2bb4007c3cc64e65174e8a9f165d
Marcus.Aspin <Marcus.Aspin@bconline.co.uk>
2018-09-14 09:12:38 +0100

    Updated to cache OID role catalogue

--- src/main/java/uk/co/bconline/ndelius/repository/oid/OIDRoleRepository.java
+++ src/main/java/uk/co/bconline/ndelius/repository/oid/OIDRoleRepository.java
@@ -2,11 +2,13 @@ package uk.co.bconline.ndelius.repository.oid;
 
 import java.util.Optional;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.ldap.repository.LdapRepository;
 
 import uk.co.bconline.ndelius.model.ldap.OIDRole;
 
 public interface OIDRoleRepository extends LdapRepository<OIDRole>
 {
+	@Cacheable("roles")
 	Optional<OIDRole> findByName(String name);
 }
