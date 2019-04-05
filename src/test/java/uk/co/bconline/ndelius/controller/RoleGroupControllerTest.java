c5ce6fd77cd4614c2efe726e07014c6a9b80bf44
marcus.aspin <marcus.aspin@bconline.co.uk>
2019-04-05 11:59:40 +0100

    Fix /rolegroup/* endpoint missing role descriptions

--- src/test/java/uk/co/bconline/ndelius/controller/RoleGroupControllerTest.java
+++ src/test/java/uk/co/bconline/ndelius/controller/RoleGroupControllerTest.java
@@ -1,12 +1,5 @@
 package uk.co.bconline.ndelius.controller;
 
-import static org.hamcrest.Matchers.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static uk.co.bconline.ndelius.test.util.AuthUtils.token;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -19,6 +12,13 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static uk.co.bconline.ndelius.test.util.AuthUtils.token;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -66,7 +66,8 @@ public class RoleGroupControllerTest
                 .andExpect(jsonPath("$", not(empty())))
                 .andExpect(jsonPath("$.name", is("AP Manager")))
                 .andExpect(jsonPath("$.roles", not(empty())))
-                .andExpect(jsonPath("$.roles[*].name", hasItem("CABT011")));
+                .andExpect(jsonPath("$.roles[*].name", hasItem("CABT011")))
+                .andExpect(jsonPath("$.roles[*].description", hasItem("Manage Case Allocation & Transfer")));
     }
 
 }
