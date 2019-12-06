622b77cef2c06354f5a6fdafdd8112b839a64169
marcus.aspin <maspin@unilink.com>
2019-12-06 11:27:29 +0000

    Added --no-sandbox flag, to support running in Docker

--- ui/src/karma.conf.js
+++ ui/src/karma.conf.js
@@ -35,7 +35,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadless'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
+    browsers: ['ChromeHeadlessNoSandbox'],
     singleRun: true
   });
 };
