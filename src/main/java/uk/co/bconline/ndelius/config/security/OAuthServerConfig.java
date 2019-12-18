81074c778029068b91b47c5cf7bed016c2a67c5a
marcus.aspin <maspin@unilink.com>
2019-12-18 16:35:03 +0000

    Use Redis for OAuth token store, with option of an embedded Redis server

--- src/main/java/uk/co/bconline/ndelius/config/security/OAuthServerConfig.java
+++ src/main/java/uk/co/bconline/ndelius/config/security/OAuthServerConfig.java
@@ -1,7 +1,9 @@
 package uk.co.bconline.ndelius.config.security;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
@@ -9,6 +11,8 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.OAuth2RequestFactory;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
 import uk.co.bconline.ndelius.security.filter.PreAuthenticatedRequestParameterFilter;
 import uk.co.bconline.ndelius.service.impl.OAuthClientDetailsService;
 
@@ -20,19 +24,23 @@ public class OAuthServerConfig extends AuthorizationServerConfigurerAdapter {
 	private String deliusSecret;
 
 	private final OAuthClientDetailsService clientDetailsService;
+	private final RedisConnectionFactory redisConnectionFactory;
 	private final PasswordEncoder passwordEncoder;
 	private OAuth2RequestFactory oauth2RequestFactory;
 
 	public OAuthServerConfig(
 			OAuthClientDetailsService clientDetailsService,
+			RedisConnectionFactory redisConnectionFactory,
 			PasswordEncoder passwordEncoder
 	) {
 		this.clientDetailsService = clientDetailsService;
+		this.redisConnectionFactory = redisConnectionFactory;
 		this.passwordEncoder = passwordEncoder;
 	}
 
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
+		endpoints.tokenStore(redisTokenStore());
 		this.oauth2RequestFactory = endpoints.getOAuth2RequestFactory();
 	}
 
@@ -49,5 +57,10 @@ public class OAuthServerConfig extends AuthorizationServerConfigurerAdapter {
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 		clients.withClientDetails(clientDetailsService);
+	}
+
+	@Bean
+	public TokenStore redisTokenStore() {
+		return new RedisTokenStore(redisConnectionFactory);
 	}
 }
