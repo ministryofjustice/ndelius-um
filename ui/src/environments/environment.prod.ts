99c3ad738485b82e1288fbecf8f5bdcd81aa1cee
marcus.aspin <maspin@unilink.com>
2020-01-27 13:15:23 +0000

    Ensure UABI* interactions are requested by UI + added some logging

--- ui/src/environments/environment.prod.ts
+++ ui/src/environments/environment.prod.ts
@@ -3,7 +3,11 @@ import {AuthConfig} from 'angular-oauth2-oidc';
 const authConfig: AuthConfig = {
   clientId: 'UserManagement-UI',
   dummyClientSecret: '',
-  scope: 'UMBI001 UMBI002 UMBI003 UMBI004 UMBI005 UMBI006 UMBI007 UMBI008 UMBI009 UMBI010 UMBI011',
+  scope:
+    // User management interactions (eg. add/update user):
+    'UMBI001 UMBI002 UMBI003 UMBI004 UMBI005 UMBI006 UMBI007 UMBI008 UMBI009 UMBI010 UMBI011 ' +
+    // Role management interactions (eg. public/private/national admin):
+    'UABI020 UABI021 UABI022 UABI023 UABI024 UABI025 UABI026',
   resource: 'NDelius',
   responseType: 'code',
   useHttpBasicAuth: true,
