9e8a35095f97103f60d15d15cec8526b177282e2
Joseph.Dundon <jdundon@unilink.com>
2020-02-12 16:19:21 +0000

    Changed username to user.getUsername() DST-6436

--- src/main/java/uk/co/bconline/ndelius/config/security/provider/token/PreAuthenticatedTokenGranter.java
+++ src/main/java/uk/co/bconline/ndelius/config/security/provider/token/PreAuthenticatedTokenGranter.java
@@ -71,7 +71,8 @@ public class PreAuthenticatedTokenGranter extends AbstractTokenGranter {
 		}
 
 		val user = userDetailsService.loadUserByUsername(username);
-		val authenticationToken = new UsernamePasswordAuthenticationToken(username, null, user.getAuthorities());
+		user.getUsername();
+		val authenticationToken = new UsernamePasswordAuthenticationToken(user.getUsername(), null, user.getAuthorities());
 		// We have to manually do the user scope filtering here, as the `checkUserScopes` flag would only work if we
 		// overrode the TokenEndpointAuthenticationFilter::extractCredentials method to include a special case for the
 		// preauthenticated grant (as well as the existing password grant)
