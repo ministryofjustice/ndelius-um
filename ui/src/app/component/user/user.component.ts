c344c3be0ece022cdad81506447091a17f8984a3
marcus.aspin <maspin@unilink.com>
2020-05-26 18:49:35 +0100

    Added warning message when changing the home area without updating staff code

--- ui/src/app/component/user/user.component.ts
+++ ui/src/app/component/user/user.component.ts
@@ -39,6 +39,7 @@ export class UserComponent implements OnInit {
   mode: string;
   params: Params;
   user: User;
+  existingHomeAreaCode: string;
   teams: Team[];
   datasets: Dataset[];
   establishments: Dataset[];
@@ -99,6 +100,7 @@ export class UserComponent implements OnInit {
         this.user = user;
         this.addSelectableRoles(user.roles || []);
         this.loaded = true;
+        this.existingHomeAreaCode = user.homeArea.code;
         this.homeAreaChanged();
         setTimeout(() => {
           this.staffCodeControl.valueChanges
