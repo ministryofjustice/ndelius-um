2b5e785fbcdceb6566aa58bf56e470667f79d821
marcus.aspin <maspin@unilink.com>
2020-05-27 20:57:20 +0100

    Added comment on disabling Redis reactive health indicator

--- src/main/java/uk/co/bconline/ndelius/UserManagementAPI.java
+++ src/main/java/uk/co/bconline/ndelius/UserManagementAPI.java
@@ -8,6 +8,9 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @SpringBootApplication(exclude = {EmbeddedLdapAutoConfiguration.class, RedisReactiveHealthContributorAutoConfiguration.class})
+// RedisReactiveHealthContributorAutoConfiguration has been excluded above due to a bug in Spring Boot 2.3.0 when
+// working with clustered Redis (https://github.com/spring-projects/spring-boot/issues/21514). This should be removed
+// once a fix is applied in Spring Boot.
 public class UserManagementAPI {
 	public static void main(String[] args) {
 		SpringApplication.run(UserManagementAPI.class, args);
