df6c6da993e1a40a0b891c052586ec6c53c73df5
marcus.aspin <maspin@unilink.com>
2020-05-27 20:27:12 +0100

    Disable reactive redis health indicator

--- src/main/java/uk/co/bconline/ndelius/UserManagementAPI.java
+++ src/main/java/uk/co/bconline/ndelius/UserManagementAPI.java
@@ -1,12 +1,13 @@
 package uk.co.bconline.ndelius;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.redis.RedisReactiveHealthContributorAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.ldap.embedded.EmbeddedLdapAutoConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-@SpringBootApplication(exclude = EmbeddedLdapAutoConfiguration.class)
+@SpringBootApplication(exclude = {EmbeddedLdapAutoConfiguration.class, RedisReactiveHealthContributorAutoConfiguration.class})
 public class UserManagementAPI {
 	public static void main(String[] args) {
 		SpringApplication.run(UserManagementAPI.class, args);
