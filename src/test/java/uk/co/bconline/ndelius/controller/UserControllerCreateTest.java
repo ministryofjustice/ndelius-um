04b3e9ead075a095ebbb88dcca8a4048408a56bf
Paul Miller <pmiller@unilink.com>
2020-08-13 17:16:26 +0100

    DST-7256 Refactor defaulting of User Preferences

--- src/test/java/uk/co/bconline/ndelius/controller/UserControllerCreateTest.java
+++ src/test/java/uk/co/bconline/ndelius/controller/UserControllerCreateTest.java
@@ -1,34 +1,12 @@
 package uk.co.bconline.ndelius.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.common.collect.ImmutableMap;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.ldap.query.SearchScope;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-import uk.co.bconline.ndelius.model.*;
-import uk.co.bconline.ndelius.model.entry.UserPreferencesEntry;
-import uk.co.bconline.ndelius.repository.ldap.UserPreferencesRepository;
-
-import java.time.LocalDate;
-import java.util.Optional;
-
 import static java.time.LocalDateTime.now;
 import static java.time.temporal.ChronoUnit.SECONDS;
 import static java.util.Arrays.asList;
 import static java.util.Collections.singletonList;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNull;
 import static org.springframework.ldap.query.LdapQueryBuilder.query;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -40,6 +18,33 @@ import static uk.co.bconline.ndelius.test.util.TokenUtils.token;
 import static uk.co.bconline.ndelius.test.util.UserUtils.aValidUser;
 import static uk.co.bconline.ndelius.test.util.UserUtils.nextTestUsername;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import javax.naming.directory.Attribute;
+import javax.naming.directory.Attributes;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.ldap.core.LdapTemplate;
+import org.springframework.ldap.query.SearchScope;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.ImmutableMap;
+
+import uk.co.bconline.ndelius.model.*;
+import uk.co.bconline.ndelius.repository.ldap.UserPreferencesRepository;
+
 @SpringBootTest
 @DirtiesContext
 @ActiveProfiles("test")
@@ -50,7 +55,7 @@ public class UserControllerCreateTest
 	private WebApplicationContext context;
 
 	@Autowired
-	private UserPreferencesRepository preferencesRepository;
+	private LdapTemplate ldapTemplate;
 
 	private MockMvc mvc;
 
@@ -163,12 +168,22 @@ public class UserControllerCreateTest
 						.build())))
 				.andExpect(status().isCreated());
 
-		Optional<UserPreferencesEntry> prefs = preferencesRepository.findOne(query()
-				.searchScope(SearchScope.ONELEVEL)
-				.base("cn=" + username + ",ou=Users")
-				.where("cn").is("UserPreferences"));
-		assertTrue(prefs.isPresent());
-		assertEquals("NRO16", prefs.get().getMostRecentlyViewedOffenders());
+		List<Attribute> attributes = ldapTemplate.search(query()
+						.searchScope(SearchScope.ONELEVEL)
+						.base("cn=" + username + ",ou=Users")
+						.where("cn")
+						.is("UserPreferences"),
+				(Attributes attrs) -> attrs.get("mostRecentlyViewedOffenders"));
+
+		assertThat(attributes, hasSize(1)); // Expect one user to be returned.
+		assertNull(attributes.get(0)); // Expect no user attributes.
+
+//		Optional<UserPreferencesEntry> prefs = preferencesRepository.findOne(query()
+//				.searchScope(SearchScope.ONELEVEL)
+//				.base("cn=" + username + ",ou=Users")
+//				.where("cn").is("UserPreferences"));
+//		assertTrue(prefs.isPresent());
+		//assertEquals("NRO16", prefs.get().getMostRecentlyViewedOffenders());
 	}
 
 	@Test
