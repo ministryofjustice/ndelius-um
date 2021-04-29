a5ada3363cda6445f7d29d1d71eefa11a655a82e
Joseph.Dundon <jdundon@unilink.com>
2021-04-29 15:55:36 +0100

    optimised imports

--- src/test/java/uk/co/bconline/ndelius/controller/UserControllerUpdateTest.java
+++ src/test/java/uk/co/bconline/ndelius/controller/UserControllerUpdateTest.java
@@ -5,7 +5,6 @@ import com.google.common.collect.ImmutableMap;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
