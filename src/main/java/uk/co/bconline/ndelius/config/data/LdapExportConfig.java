66c453a72daac2dec1b70baf9d98f1da8ad0873d
marcus.aspin <maspin@unilink.com>
2021-06-07 18:34:43 +0100

    Set export username from config

--- src/main/java/uk/co/bconline/ndelius/config/data/LdapExportConfig.java
+++ src/main/java/uk/co/bconline/ndelius/config/data/LdapExportConfig.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.ldap.LdapAutoConfiguration;
 import org.springframework.boot.autoconfigure.ldap.LdapProperties;
 import org.springframework.context.annotation.Bean;
@@ -17,11 +18,14 @@ import org.springframework.ldap.core.support.LdapContextSource;
 @Slf4j
 @Configuration
 public class LdapExportConfig extends LdapAutoConfiguration {
+	@Value("${spring.ldap.export.username:${spring.ldap.username}}")
+	private String exportUser;
+
 	@Override
 	@Bean("exportLdapContextSource")
 	public LdapContextSource ldapContextSource(LdapProperties properties, Environment environment,
 											   ObjectProvider<DirContextAuthenticationStrategy> dirContextAuthenticationStrategy) {
-		properties.setUsername(properties.getUsername().replace("admin", "root"));
+		properties.setUsername(exportUser);
 		val ctxSource = super.ldapContextSource(properties, environment, dirContextAuthenticationStrategy);
 		val pooled = Boolean.parseBoolean(properties.getBaseEnvironment().getOrDefault("com.sun.jndi.ldap.connect.pool", "false"));
 		ctxSource.setPooled(pooled);
@@ -31,7 +35,7 @@ public class LdapExportConfig extends LdapAutoConfiguration {
 	@Override
 	@Bean("exportLdapTemplate")
 	public LdapTemplate ldapTemplate(LdapProperties properties, @Qualifier("exportLdapContextSource") ContextSource contextSource) {
-		properties.setUsername(properties.getUsername().replace("admin", "root"));
+		properties.setUsername(exportUser);
 		return super.ldapTemplate(properties, contextSource);
 	}
 }
