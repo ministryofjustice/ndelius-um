fbbc4f3dcd3e30e9319f74f1ffb0fa6830dc1b0f
marcus.aspin <maspin@unilink.com>
2021-09-08 18:21:31 +0100

    Added Gatling performance tests

---
+++ src/gatling/scala/Config.scala
@@ -0,0 +1,16 @@
+object Config {
+  val baseUrl: String = sys.env("BASE_URL")
+  val username: String = sys.env.getOrElse("USERNAME", null)
+  val password: String = sys.env("PASSWORD")
+  val concurrentUsers: Int = sys.env.getOrElse("CONCURRENT_USERS", "50").toInt
+  val duration: Int = sys.env.getOrElse("DURATION", "0").toInt // Set to 0 to run once
+  val rampUpDuration: Int = sys.env.getOrElse("RAMP_UP_DURATION", (duration / 8).toString).toInt
+
+
+  // Get the following values from CloudWatch logs/metrics in Production:
+  val observedConcurrentUsers: Int = 50
+  val observedRequestsPerMinute: Int = 15
+
+  val actualRequestsPerMinute: Float = observedRequestsPerMinute * concurrentUsers / observedConcurrentUsers
+  val actualRequestsPerSecond: Float = actualRequestsPerMinute / 60
+}
---
+++ src/gatling/scala/Data.scala
@@ -0,0 +1,16 @@
+import java.util.concurrent.ConcurrentLinkedQueue
+import scala.util.Random
+
+object Data {
+  val usernames: Iterator[Map[String, String]] =
+    if (Config.username != null) {
+      // Use a single username if one is configured
+      Iterator.single(Map("username" -> Config.username))
+    } else {
+      // Otherwise use a random username of the format nd.perf09999
+      Iterator.continually(Map("username" -> "nd.perf0%04d".format(Random.nextInt(10000))))
+    }
+
+  val userResponseBodyQueue = new ConcurrentLinkedQueue[Map[String, String]]()
+  val userResponseBody: Iterator[Map[String, String]] = Iterator.continually(userResponseBodyQueue.poll())
+}
