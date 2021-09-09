601d5be8088ee93b3635ad269fbab7899e8ea93c
marcus.aspin <maspin@unilink.com>
2021-09-09 10:42:20 +0100

    Added details of Gatling load tests

--- src/gatling/scala/Config.scala
+++ src/gatling/scala/Config.scala
@@ -1,7 +1,7 @@
 object Config {
   val baseUrl: String = sys.env("BASE_URL")
-  val username: String = sys.env.getOrElse("USERNAME", null)
-  val password: String = sys.env("PASSWORD")
+  val username: String = sys.env.getOrElse("TEST_USERNAME", null)
+  val password: String = sys.env("TEST_PASSWORD")
   val concurrentUsers: Int = sys.env.getOrElse("CONCURRENT_USERS", "50").toInt
   val duration: Int = sys.env.getOrElse("DURATION", "0").toInt // Set to 0 to run once
   val rampUpDuration: Int = sys.env.getOrElse("RAMP_UP_DURATION", (duration / 8).toString).toInt
