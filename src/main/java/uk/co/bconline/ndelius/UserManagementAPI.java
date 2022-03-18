0d3844911f9385e85bb3fa9e1f2c1e23a84b7e79
Marcus Aspin <maspin@unilink.com>
2022-03-18 12:04:05 +0000

    Upgrade Spring Boot to 2.6.4 and H2 to 2.1.210 (#615)

--- src/main/java/uk/co/bconline/ndelius/UserManagementAPI.java
+++ src/main/java/uk/co/bconline/ndelius/UserManagementAPI.java
@@ -1,16 +1,22 @@
 package uk.co.bconline.ndelius;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.autoconfigure.redis.RedisReactiveHealthContributorAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.ldap.embedded.EmbeddedLdapAutoConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Delius User Management Tool (UMT)",
+				description = "REST API to enable NPS-ICT to effectively create and maintain user accounts in the National Delius application, and to allow external services to securely authenticate Probation staff.",
+				contact = @Contact(name = "Unilink", url = "https://unilink.com")),
+		externalDocs = @ExternalDocumentation(description = "GitHub", url = "https://github.com/ministryofjustice/ndelius-um"))
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-@SpringBootApplication(exclude = {EmbeddedLdapAutoConfiguration.class, RedisReactiveHealthContributorAutoConfiguration.class})
-// RedisReactiveHealthContributorAutoConfiguration has been excluded above due to a bug in Spring Boot 2.3.0 when
-// working with clustered Redis (https://github.com/spring-projects/spring-boot/issues/21514). This should be removed
-// once a fix is applied in Spring Boot.
+@SpringBootApplication(exclude = {EmbeddedLdapAutoConfiguration.class})
 public class UserManagementAPI {
 	public static void main(String[] args) {
 		SpringApplication.run(UserManagementAPI.class, args);
