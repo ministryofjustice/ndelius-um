71d24930286cd63c694ce77865d3c3926b6debf3
Robert.McCormack <rmccormack@unilink.com>
2024-06-17 15:29:38 +0100

    Angular -> 18

--- ui/src/app/module/app.routing.module.ts
+++ ui/src/app/module/app.routing.module.ts
@@ -15,7 +15,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { onSameUrlNavigation: 'reload', relativeLinkResolution: 'legacy' })],
+  imports: [RouterModule.forRoot(routes, { onSameUrlNavigation: 'reload'})],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
